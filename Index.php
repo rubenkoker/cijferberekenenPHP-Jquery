ae08cb80ec67a62964861c8149a916dda1afcb3e
Ruben Koker <ruben.koker@itvitaelearning.nl>
2024-12-02 18:04:34 +0100

    begin van form

--- Index.php
+++ Index.php
@@ -3,5 +3,12 @@ function CijferMetNormering($goedeVragen,$totaalVragen, $normering): float|int
 {
     return ($goedeVragen/$totaalVragen)*9 + $normering;
 }
-echo CijferMetNormering(goedeVragen: 39, totaalVragen:71,normering: 1);
+echo CijferMetNormering(goedeVragen: 38, totaalVragen:71,normering: 1);
+
+
 ?>
+<html>
+   <form action="" method="POST">
+        <input type="text" name = "goedevragen">
+   </form> 
+</html>
