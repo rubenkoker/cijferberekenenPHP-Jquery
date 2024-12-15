e47499f063fcdd84a103e9fe09ad3672479bd99e
Ruben Koker <ruben.koker@itvitaelearning.nl>
2024-12-15 14:36:12 +0100

    the button works

--- Index.php
+++ Index.php
@@ -3,14 +3,14 @@ function CijferMetNormering($goedeVragen,$totaalVragen, $normering): float|int
 {
     return round(($goedeVragen/$totaalVragen)*9 + $normering,precision: 1);
 }
-echo CijferMetNormering(goedeVragen: 38, totaalVragen:71,normering: 1);
+//echo CijferMetNormering(goedeVragen: 38, totaalVragen:71,normering: 1);
 
 
 ?>
 <?php if (!empty($_POST)): ?>
     <?php
     if(isset($_POST["totaalvragen"])) {
-     echo(CijferMetNormering($_POST["goedevragen"],$_POST["totaalvragen"],$POST["normering"]));
+     echo(CijferMetNormering($_POST["goedevragen"],$_POST["totaalvragen"],normering: $_POST["normering"]));
     }
     ?>
 <?php endif; ?>
